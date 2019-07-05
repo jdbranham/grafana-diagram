84250d44edbbacb4a504bd55b302981b18c94f7a
Jeremy Branham <Jeremy@Savantly.net>
2019-07-05 18:03:49 -0500

    updated dist

--- dist/diagramControl.js
+++ dist/diagramControl.js
@@ -103,6 +103,7 @@ System.register(['./libs/mermaid/dist/mermaid', './libs/d3/dist/d3.min', 'app/co
         thresholds: '0,10',
         decimals: 2, // decimal precision
         colors: ['rgba(50, 172, 45, 0.97)', 'rgba(237, 129, 40, 0.89)', 'rgba(245, 54, 54, 0.9)'],
+        style: '',
         legend: {
           show: true,
           min: true,
@@ -133,7 +134,7 @@ System.register(['./libs/mermaid/dist/mermaid', './libs/d3/dist/d3.min', 'app/co
         mermaidServiceUrl: '',
         init: {
           logLevel: 3, //1:debug, 2:info, 3:warn, 4:error, 5:fatal
-          cloneCssStyles: false, // - This options controls whether or not the css rules should be copied into the generated svg
+          cloneCssStyles: true, // - This options controls whether or not the css rules should be copied into the generated svg
           startOnLoad: false, // - This options controls whether or mermaid starts when the page loads
           arrowMarkerAbsolute: true, // - This options controls whether or arrow markers in html code will be absolute paths or an anchor, #. This matters if you are using base tag settings.
           flowchart: {
@@ -773,6 +774,12 @@ System.register(['./libs/mermaid/dist/mermaid', './libs/d3/dist/d3.min', 'app/co
               v.html(seriesItem.valueFormatted);
             }
 
+            function injectCustomStyle(ctrl) {
+              var diagramDiv = d3.select(document.getElementById(ctrl.panel.graphId));
+              var styleElement = diagramDiv.append('style');
+              styleElement.text(ctrl.panel.style);
+            }
+
             function updateStyle() {
               var data = ctrl.svgData;
               ctrl.svgData = {}; // get rid of the data after consuming it. This prevents adding duplicate DOM elements
@@ -812,7 +819,8 @@ System.register(['./libs/mermaid/dist/mermaid', './libs/d3/dist/d3.min', 'app/co
 
                 console.debug('couldnt not find a diagram node with id/text: ' + key);
               }
-              //return $(svg).html();
+
+              injectCustomStyle(ctrl);
             } // End updateStyle()
 
             function render() {
