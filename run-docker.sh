0947e2bd6d0b7c290c7b8d88e5ba3ad2df94c55b
Jeremy Branham <Jeremy.Branham@gigsternetwork.com>
2021-06-06 02:16:52 -0500

    updates test container to grafana 7.3.7

--- run-docker.sh
+++ run-docker.sh
@@ -1,1 +1,1 @@
-docker run -p 3000:3000 -v "$(pwd)":/var/lib/grafana/plugins/jdbranham-diagram-panel --name=grafana grafana/grafana:7.1.5
+docker run -p 3000:3000 -v "$(pwd)":/var/lib/grafana/plugins/jdbranham-diagram-panel --name=grafana grafana/grafana:7.3.7
